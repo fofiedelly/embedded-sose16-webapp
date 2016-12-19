f80e52ceaae35297b9ab585fe761088e8ff358d2
Delly Fofie <delly.fofie@gmail.com>
2016-12-19 14:53:53 +0100

    constant changed

--- app/js/constants.js
+++ app/js/constants.js
@@ -1,7 +1,7 @@
 const AppSettings = {
     appTitle: 'Home Automation',
     // apiUrl: 'http://localhost:8081'
-    apiUrl: 'https://backend-seb-embedded-ws16-17.herokuapp.com/rooms',
+    apiUrl: 'https://backend-seb-embedded-ws16-17.herokuapp.com',
     tempCurvNumberOfTimestamps: 10
 };
 
